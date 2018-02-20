bf5fd159015ec9d7b22c180e05cfea5ddde1f091
Osamak063 <osamak063@gmail.com>
2018-02-20 17:15:09 +0500

    Holiday Feature implemented.

--- app/src/main/java/com/example/osamakhalid/schoolsystem/Model/HolidayResponseList.java
+++ app/src/main/java/com/example/osamakhalid/schoolsystem/Model/HolidayResponseList.java
@@ -1,8 +1,18 @@
 package com.example.osamakhalid.schoolsystem.Model;
 
+import com.google.gson.annotations.SerializedName;
+
+import java.util.List;
+
 /**
  * Created by Osama Khalid on 2/20/2018.
  */
 
 public class HolidayResponseList {
+    @SerializedName("holiday_data")
+    private List<HolidayResponse> holidayList;
+
+    public List<HolidayResponse> getHolidayList() {
+        return holidayList;
+    }
 }
