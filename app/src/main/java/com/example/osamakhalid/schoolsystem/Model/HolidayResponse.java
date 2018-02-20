ca948fd799e653f323ce6d39bcc2674570b5bcf1
Osamak063 <osamak063@gmail.com>
2018-02-20 17:15:02 +0500

    Holiday Feature implemented.

--- app/src/main/java/com/example/osamakhalid/schoolsystem/Model/HolidayResponse.java
+++ app/src/main/java/com/example/osamakhalid/schoolsystem/Model/HolidayResponse.java
@@ -1,8 +1,77 @@
 package com.example.osamakhalid.schoolsystem.Model;
 
+import com.google.gson.annotations.Expose;
+import com.google.gson.annotations.SerializedName;
+
 /**
  * Created by Osama Khalid on 2/19/2018.
  */
 
 public class HolidayResponse {
+    @SerializedName("holiday_id")
+    @Expose
+    private String holidayId;
+    @SerializedName("title")
+    @Expose
+    private String title;
+    @SerializedName("from_date")
+    @Expose
+    private String fromDate;
+    @SerializedName("to_date")
+    @Expose
+    private String toDate;
+    @SerializedName("details")
+    @Expose
+    private String details;
+    @SerializedName("image")
+    @Expose
+    private String image;
+
+    public String getHolidayId() {
+        return holidayId;
+    }
+
+    public void setHolidayId(String holidayId) {
+        this.holidayId = holidayId;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getFromDate() {
+        return fromDate;
+    }
+
+    public void setFromDate(String fromDate) {
+        this.fromDate = fromDate;
+    }
+
+    public String getToDate() {
+        return toDate;
+    }
+
+    public void setToDate(String toDate) {
+        this.toDate = toDate;
+    }
+
+    public String getDetails() {
+        return details;
+    }
+
+    public void setDetails(String details) {
+        this.details = details;
+    }
+
+    public String getImage() {
+        return image;
+    }
+
+    public void setImage(String image) {
+        this.image = image;
+    }
 }
