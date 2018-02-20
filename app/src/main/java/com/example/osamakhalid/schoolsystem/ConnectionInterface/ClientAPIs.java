2d0b94507c2766c60900d96d31ad07edd5f1373f
Osamak063 <osamak063@gmail.com>
2018-02-20 17:12:48 +0500

    Holiday Feature implemented.

--- app/src/main/java/com/example/osamakhalid/schoolsystem/ConnectionInterface/ClientAPIs.java
+++ app/src/main/java/com/example/osamakhalid/schoolsystem/ConnectionInterface/ClientAPIs.java
@@ -2,6 +2,8 @@ package com.example.osamakhalid.schoolsystem.ConnectionInterface;
 
 import com.example.osamakhalid.schoolsystem.APIs.ConnectionURLS;
 import com.example.osamakhalid.schoolsystem.Model.Exam_Model;
+import com.example.osamakhalid.schoolsystem.Model.HolidayResponse;
+import com.example.osamakhalid.schoolsystem.Model.HolidayResponseList;
 import com.example.osamakhalid.schoolsystem.Model.LoginResponse;
 import com.example.osamakhalid.schoolsystem.Model.NewsAndEventsResponse;
 import com.example.osamakhalid.schoolsystem.Model.NewsAndEventsResponseList;
@@ -12,6 +14,7 @@ import retrofit2.http.FormUrlEncoded;
 import retrofit2.http.GET;
 import retrofit2.http.Header;
 import retrofit2.http.POST;
+import retrofit2.http.Query;
 
 
 public interface ClientAPIs {
@@ -27,5 +30,7 @@ public interface ClientAPIs {
     @GET(ConnectionURLS.NEWS_AND_EVENTS_URL)
     Call<NewsAndEventsResponseList> getNewsAndEvents(@Header("Authorization") String authHeader);
 
+    @GET(ConnectionURLS.HOLIDAY_URL)
+    Call<HolidayResponseList> getHolidays(@Query("type") String type, @Query("lang") String lang, @Header("Authorization") String authHeader);
 }
 
