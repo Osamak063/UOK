9a75604d826674d412e0dc15a0df8a6040b0efb6
hamzaahmed800 <hamzaahmed800@gmail.com>
2018-02-21 19:13:46 +0500

    "H_classes"

---
+++ app/src/main/java/com/example/osamakhalid/schoolsystem/Model/Homework_Data_Model.java
@@ -0,0 +1,59 @@
+package com.example.osamakhalid.schoolsystem.Model;
+
+import com.google.gson.annotations.Expose;
+import com.google.gson.annotations.SerializedName;
+
+/**
+ * Created by HAMI on 21/02/2018.
+ */
+
+public class Homework_Data_Model {
+
+    @SerializedName("date")
+    @Expose
+    private String date;
+    @SerializedName("section")
+    @Expose
+    private String section;
+    @SerializedName("subject")
+    @Expose
+    private String subject;
+    @SerializedName("desc")
+    @Expose
+    private String desc;
+
+    public String getSection() {
+        return section;
+    }
+
+    public void setSection(String section) {
+        this.section = section;
+    }
+
+    public String getSubject() {
+        return subject;
+    }
+
+    public void setSubject(String subject) {
+        this.subject = subject;
+    }
+
+    public String getDesc() {
+        return desc;
+    }
+
+    public void setDesc(String desc) {
+        this.desc = desc;
+    }
+
+    public String getDate() {
+        return date;
+    }
+
+    public void setDate(String date) {
+        this.date = date;
+    }
+
+
+
+}
