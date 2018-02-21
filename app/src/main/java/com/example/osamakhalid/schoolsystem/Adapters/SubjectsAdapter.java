deb2d6ebcf52f4921b6134d29b355230f0b51668
Osamak063 <osamak063@gmail.com>
2018-02-21 17:58:00 +0500

    Subjects implemented.

--- app/src/main/java/com/example/osamakhalid/schoolsystem/Adapters/SubjectsAdapter.java
+++ app/src/main/java/com/example/osamakhalid/schoolsystem/Adapters/SubjectsAdapter.java
@@ -1,8 +1,68 @@
 package com.example.osamakhalid.schoolsystem.Adapters;
 
+import android.content.Context;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+import com.example.osamakhalid.schoolsystem.Model.Alert_Model;
+import com.example.osamakhalid.schoolsystem.Model.SubjectResponse;
+import com.example.osamakhalid.schoolsystem.R;
+
+import java.util.List;
+
 /**
  * Created by Osama Khalid on 2/21/2018.
  */
 
-public class SubjectsAdapter {
+public class SubjectsAdapter extends RecyclerView.Adapter<SubjectsAdapter.ViewHolder> {
+    Context context;
+    List<SubjectResponse> subjectModels;
+
+    public SubjectsAdapter(List<SubjectResponse> subjectModels, Context context) {
+        this.context = context;
+        this.subjectModels = subjectModels;
+    }
+
+    @Override
+    public SubjectsAdapter.ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        View v = LayoutInflater.from(parent.getContext()).inflate(R.layout.subject_view_layout, parent, false);
+        return new SubjectsAdapter.ViewHolder(v);
+    }
+
+    @Override
+    public void onBindViewHolder(SubjectsAdapter.ViewHolder holder, int position) {
+
+        final SubjectResponse subjectModel = subjectModels.get(position);
+        holder.name.setText("Name: " + subjectModel.getSubjectName());
+        holder.code.setText("Code: " + subjectModel.getSubjectCode());
+        holder.author.setText("Author: " + subjectModel.getSubjectAuthor());
+        holder.teacher.setText("Teacher: " + subjectModel.getTeacher());
+    }
+
+    @Override
+    public int getItemCount() {
+        return subjectModels.size();
+    }
+
+
+    public class ViewHolder extends RecyclerView.ViewHolder {
+
+        TextView name, author, code, teacher;
+
+        public ViewHolder(View itemView) {
+            super(itemView);
+            name = itemView.findViewById(R.id.subject_name);
+            author = itemView.findViewById(R.id.subject_author);
+            code = itemView.findViewById(R.id.subject_code);
+            teacher = itemView.findViewById(R.id.subject_teacher);
+
+
+        }
+
+    }
+
 }
