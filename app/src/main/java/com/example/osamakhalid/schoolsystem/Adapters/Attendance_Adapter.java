1e4ff834984f28c38aac389f2554a8b597072ec2
Osamak063 <osamak063@gmail.com>
2018-02-23 19:08:08 +0500

    Attendance feature implemented.

--- app/src/main/java/com/example/osamakhalid/schoolsystem/Adapters/Attendance_Adapter.java
+++ app/src/main/java/com/example/osamakhalid/schoolsystem/Adapters/Attendance_Adapter.java
@@ -2,6 +2,7 @@ package com.example.osamakhalid.schoolsystem.Adapters;
 
 import android.content.Context;
 import android.graphics.Color;
+import android.graphics.drawable.Drawable;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -9,8 +10,7 @@ import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.TextView;
 
-import com.example.osamakhalid.schoolsystem.Model.Attendance_Model;
-import com.example.osamakhalid.schoolsystem.Model.Transport_Model;
+import com.example.osamakhalid.schoolsystem.Model.AttendanceValuesResponse;
 import com.example.osamakhalid.schoolsystem.R;
 
 import java.util.List;
@@ -21,9 +21,9 @@ import java.util.List;
 
 public class Attendance_Adapter extends RecyclerView.Adapter<Attendance_Adapter.ViewHolder> {
     Context context;
-    List<Attendance_Model> attendanceModels;
+    List<AttendanceValuesResponse> attendanceModels;
 
-    public Attendance_Adapter(List<Attendance_Model> attendanceModels, Context context) {
+    public Attendance_Adapter(List<AttendanceValuesResponse> attendanceModels, Context context) {
         this.context = context;
         this.attendanceModels = attendanceModels;
     }
@@ -37,15 +37,21 @@ public class Attendance_Adapter extends RecyclerView.Adapter<Attendance_Adapter.
     @Override
     public void onBindViewHolder(Attendance_Adapter.ViewHolder holder, int position) {
 
-        final Attendance_Model attendance_model = attendanceModels.get(position);
-        holder.date.setText(attendance_model.getDate());
-        if (attendance_model.getAttendance().equals("P")) {
+        final AttendanceValuesResponse attendance_model = attendanceModels.get(position);
+        holder.date.setText(attendance_model.getDayName() + "," + attendance_model.getMonth() + " " + attendance_model.getDay());
+        holder.present.setTextColor(Color.parseColor("#43A047"));
+        holder.present.setBackground(context.getResources().getDrawable(R.drawable.button_bg_stroke_present));
+        holder.absent.setTextColor(Color.parseColor("#E53935"));
+        holder.absent.setBackground(context.getResources().getDrawable(R.drawable.button_bg_stroke_absent));
+        holder.holiday.setTextColor(Color.parseColor("#039BE5"));
+        holder.holiday.setBackground(context.getResources().getDrawable(R.drawable.button_bg_stroke_holiday));
+        if (attendance_model.getAttendanceStatus().equals("P")) {
             holder.present.setTextColor(Color.WHITE);
             holder.present.setBackgroundColor(Color.parseColor("#43A047"));
-        } else if (attendance_model.getAttendance().equals("A")) {
+        } else if (attendance_model.getAttendanceStatus().equals("A")) {
             holder.absent.setTextColor(Color.WHITE);
             holder.absent.setBackgroundColor(Color.parseColor("#E53935"));
-        } else if (attendance_model.getAttendance().equals("H")) {
+        } else if (attendance_model.getAttendanceStatus().equals("H")) {
             holder.holiday.setTextColor(Color.WHITE);
             holder.holiday.setBackgroundColor(Color.parseColor("#039BE5"));
         }
