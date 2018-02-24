0d81f1ba375bc8d88dcfd887fcb3f2172cfff0a7
Osamak063 <osamak063@gmail.com>
2018-02-24 17:29:58 +0500

    Exam Schedule implemented.

--- app/src/main/java/com/example/osamakhalid/schoolsystem/Model/ExamScheduleResponse.java
+++ app/src/main/java/com/example/osamakhalid/schoolsystem/Model/ExamScheduleResponse.java
@@ -1,8 +1,77 @@
 package com.example.osamakhalid.schoolsystem.Model;
 
+import com.google.gson.annotations.Expose;
+import com.google.gson.annotations.SerializedName;
+
 /**
  * Created by Osama Khalid on 2/24/2018.
  */
 
 public class ExamScheduleResponse {
+    @SerializedName("exam_name")
+    @Expose
+    private String examName;
+    @SerializedName("class")
+    @Expose
+    private String _class;
+    @SerializedName("subject")
+    @Expose
+    private String subject;
+    @SerializedName("date")
+    @Expose
+    private String date;
+    @SerializedName("time")
+    @Expose
+    private String time;
+    @SerializedName("room")
+    @Expose
+    private String room;
+
+    public String getExamName() {
+        return examName;
+    }
+
+    public void setExamName(String examName) {
+        this.examName = examName;
+    }
+
+    public String getClass_() {
+        return _class;
+    }
+
+    public void setClass_(String _class) {
+        this._class = _class;
+    }
+
+    public String getSubject() {
+        return subject;
+    }
+
+    public void setSubject(String subject) {
+        this.subject = subject;
+    }
+
+    public String getDate() {
+        return date;
+    }
+
+    public void setDate(String date) {
+        this.date = date;
+    }
+
+    public String getTime() {
+        return time;
+    }
+
+    public void setTime(String time) {
+        this.time = time;
+    }
+
+    public String getRoom() {
+        return room;
+    }
+
+    public void setRoom(String room) {
+        this.room = room;
+    }
 }
