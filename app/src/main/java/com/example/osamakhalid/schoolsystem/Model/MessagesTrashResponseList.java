ea1ecdee872054841ae79944599e163cc46babf4
Osamak063 <osamak063@gmail.com>
2018-03-01 21:15:53 +0500

    Messages feature implemented.

--- app/src/main/java/com/example/osamakhalid/schoolsystem/Model/MessagesTrashResponseList.java
+++ app/src/main/java/com/example/osamakhalid/schoolsystem/Model/MessagesTrashResponseList.java
@@ -1,8 +1,24 @@
 package com.example.osamakhalid.schoolsystem.Model;
 
+import com.google.gson.annotations.Expose;
+import com.google.gson.annotations.SerializedName;
+
+import java.util.List;
+
 /**
  * Created by Osama Khalid on 2/28/2018.
  */
 
 public class MessagesTrashResponseList {
+    @SerializedName("message_data")
+    @Expose
+    private List<MessagesTrashResponse> messageData = null;
+
+    public List<MessagesTrashResponse> getMessageData() {
+        return messageData;
+    }
+
+    public void setMessageData(List<MessagesTrashResponse> messageData) {
+        this.messageData = messageData;
+    }
 }
