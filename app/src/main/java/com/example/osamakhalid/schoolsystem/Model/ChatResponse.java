81eded3d1cb4c326eb1564b719d476766f9f1a8a
Osamak063 <osamak063@gmail.com>
2018-03-01 21:12:46 +0500

    Messages feature implemented.

--- app/src/main/java/com/example/osamakhalid/schoolsystem/Model/ChatResponse.java
+++ app/src/main/java/com/example/osamakhalid/schoolsystem/Model/ChatResponse.java
@@ -1,8 +1,91 @@
 package com.example.osamakhalid.schoolsystem.Model;
 
+import com.google.gson.annotations.Expose;
+import com.google.gson.annotations.SerializedName;
+
+import java.util.List;
+
 /**
  * Created by Osama Khalid on 3/1/2018.
  */
 
 public class ChatResponse {
+    @SerializedName("from")
+    @Expose
+    private String from;
+    @SerializedName("to")
+    @Expose
+    private String to;
+    @SerializedName("subject")
+    @Expose
+    private String subject;
+    @SerializedName("attachment")
+    @Expose
+    private String attachment;
+    @SerializedName("attachment_name")
+    @Expose
+    private String attachmentName;
+    @SerializedName("messageid")
+    @Expose
+    private String messageid;
+    @SerializedName("messages_data")
+    @Expose
+    private List<ChatDataResponse> messagesData = null;
+
+    public String getFrom() {
+        return from;
+    }
+
+    public void setFrom(String from) {
+        this.from = from;
+    }
+
+    public String getTo() {
+        return to;
+    }
+
+    public void setTo(String to) {
+        this.to = to;
+    }
+
+    public String getSubject() {
+        return subject;
+    }
+
+    public void setSubject(String subject) {
+        this.subject = subject;
+    }
+
+    public String getAttachment() {
+        return attachment;
+    }
+
+    public void setAttachment(String attachment) {
+        this.attachment = attachment;
+    }
+
+    public String getAttachmentName() {
+        return attachmentName;
+    }
+
+    public void setAttachmentName(String attachmentName) {
+        this.attachmentName = attachmentName;
+    }
+
+    public String getMessageid() {
+        return messageid;
+    }
+
+    public void setMessageid(String messageid) {
+        this.messageid = messageid;
+    }
+
+    public List<ChatDataResponse> getMessagesData() {
+        return messagesData;
+    }
+
+    public void setMessagesData(List<ChatDataResponse> messagesData) {
+        this.messagesData = messagesData;
+    }
+
 }
