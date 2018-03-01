57d3ed376bd13a0c03388c103cbb0cf7347116c0
Osamak063 <osamak063@gmail.com>
2018-03-01 21:14:59 +0500

    Messages feature implemented.

--- app/src/main/java/com/example/osamakhalid/schoolsystem/Model/MessagesFavResponseList.java
+++ app/src/main/java/com/example/osamakhalid/schoolsystem/Model/MessagesFavResponseList.java
@@ -1,8 +1,25 @@
 package com.example.osamakhalid.schoolsystem.Model;
 
+import com.google.gson.annotations.Expose;
+import com.google.gson.annotations.SerializedName;
+
+import java.util.List;
+
 /**
  * Created by Osama Khalid on 2/28/2018.
  */
 
 public class MessagesFavResponseList {
+
+    @SerializedName("message_data")
+    @Expose
+    private List<MessagesFavResponse> messageData = null;
+
+    public List<MessagesFavResponse> getMessageData() {
+        return messageData;
+    }
+
+    public void setMessageData(List<MessagesFavResponse> messageData) {
+        this.messageData = messageData;
+    }
 }
