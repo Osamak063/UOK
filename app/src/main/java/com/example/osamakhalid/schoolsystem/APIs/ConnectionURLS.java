c9c0a996cf7d4dd118cc2d9df58a667e85ccc541
Osamak063 <osamak063@gmail.com>
2018-03-01 21:13:10 +0500

    Messages feature implemented.

--- app/src/main/java/com/example/osamakhalid/schoolsystem/APIs/ConnectionURLS.java
+++ app/src/main/java/com/example/osamakhalid/schoolsystem/APIs/ConnectionURLS.java
@@ -18,11 +18,16 @@ public class ConnectionURLS {
     public static final String HOMEWORK_URL = "http://demo.simsportal.com/api/student/homework_by_date/";
 
     public static final String ATTENDANCE_URL = "student/student_attendence/id/1/type/Student/username/student01/lang/english";
-    public static final String EXAM_SCHEDULE_URL= "student/exams_schedule/username/student01/type/Student";
-    public static final String SUBJECTS_URL="student/subjects/username/student01/type/Student";
+    public static final String EXAM_SCHEDULE_URL = "student/exams_schedule/username/student01/type/Student";
+    public static final String SUBJECTS_URL = "student/subjects/username/student01/type/Student";
 
     public static final String TEACHER_DETAILS_URL = "http://demo.simsportal.com/api/student/teachers/";
 
     public static final String TEACHER_PERSONAL_PROFILE = "http://demo.simsportal.com/api/student/teacher_profile/";
 
+    public static final String MESSAGE_INBOX_URL = "student/message_inbox/";
+    public static final String MESSAGE_SENT_URL = "student/message_sent/";
+    public static final String MESSAGE_FAV_URL = "student/fav_message/";
+    public static final String MESSAGE_TRASH_URL = "student/trash_message/";
+    public static final String MESSAGE_CHAT_URL = "student/message_view/";
 }
