0d6ec15a37b78476483f6b343ce1e721a8c0b789
Osamak063 <osamak063@gmail.com>
2018-03-03 19:49:44 +0500

    attachment with leaves feature

--- app/src/main/java/com/example/osamakhalid/schoolsystem/Model/LeavesResponse.java
+++ app/src/main/java/com/example/osamakhalid/schoolsystem/Model/LeavesResponse.java
@@ -1,8 +1,67 @@
 package com.example.osamakhalid.schoolsystem.Model;
 
+import com.google.gson.annotations.Expose;
+import com.google.gson.annotations.SerializedName;
+
 /**
  * Created by Osama Khalid on 3/3/2018.
  */
 
 public class LeavesResponse {
+    @SerializedName("id")
+    @Expose
+    private String id;
+    @SerializedName("to")
+    @Expose
+    private String to;
+    @SerializedName("submit_date")
+    @Expose
+    private String submitDate;
+    @SerializedName("application_title")
+    @Expose
+    private String applicationTitle;
+    @SerializedName("status")
+    @Expose
+    private String status;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getTo() {
+        return to;
+    }
+
+    public void setTo(String to) {
+        this.to = to;
+    }
+
+    public String getSubmitDate() {
+        return submitDate;
+    }
+
+    public void setSubmitDate(String submitDate) {
+        this.submitDate = submitDate;
+    }
+
+    public String getApplicationTitle() {
+        return applicationTitle;
+    }
+
+    public void setApplicationTitle(String applicationTitle) {
+        this.applicationTitle = applicationTitle;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
 }
--- app/src/main/java/com/example/osamakhalid/schoolsystem/Model/LeavesResponseList.java
+++ app/src/main/java/com/example/osamakhalid/schoolsystem/Model/LeavesResponseList.java
@@ -1,8 +1,24 @@
 package com.example.osamakhalid.schoolsystem.Model;
 
+import com.google.gson.annotations.Expose;
+import com.google.gson.annotations.SerializedName;
+
+import java.util.List;
+
 /**
  * Created by Osama Khalid on 3/3/2018.
  */
 
 public class LeavesResponseList {
+    @SerializedName("leaves_data")
+    @Expose
+    private List<LeavesResponse> leavesData = null;
+
+    public List<LeavesResponse> getLeavesData() {
+        return leavesData;
+    }
+
+    public void setLeavesData(List<LeavesResponse> leavesData) {
+        this.leavesData = leavesData;
+    }
 }
