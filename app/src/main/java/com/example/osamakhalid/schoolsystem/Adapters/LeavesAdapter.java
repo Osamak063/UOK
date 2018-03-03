1af0359fbacde4a13abe67872bf70f73bb2b73d1
Osamak063 <osamak063@gmail.com>
2018-03-03 19:49:15 +0500

    attachment with leaves feature

---
+++ app/src/main/java/com/example/osamakhalid/schoolsystem/Adapters/LeavesAdapter.java
@@ -0,0 +1,8 @@
+package com.example.osamakhalid.schoolsystem.Adapters;
+
+/**
+ * Created by Osama Khalid on 3/3/2018.
+ */
+
+public class LeavesAdapter {
+}
---
+++ app/src/main/java/com/example/osamakhalid/schoolsystem/Model/LeavesResponseList.java
@@ -0,0 +1,8 @@
+package com.example.osamakhalid.schoolsystem.Model;
+
+/**
+ * Created by Osama Khalid on 3/3/2018.
+ */
+
+public class LeavesResponseList {
+}
