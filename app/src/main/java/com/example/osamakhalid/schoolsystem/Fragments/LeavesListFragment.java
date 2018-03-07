ee0389e9e428ecfe33b0f95a48ffd8c8364a644a
Osamak063 <osamak063@gmail.com>
2018-03-07 17:39:59 +0500

    Leaves with submit leave

--- app/src/main/java/com/example/osamakhalid/schoolsystem/Fragments/LeavesListFragment.java
+++ app/src/main/java/com/example/osamakhalid/schoolsystem/Fragments/LeavesListFragment.java
@@ -1,109 +1,96 @@
 package com.example.osamakhalid.schoolsystem.Fragments;
 
+import android.app.ProgressDialog;
 import android.content.Context;
 import android.net.Uri;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.util.Base64;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Toast;
 
+import com.example.osamakhalid.schoolsystem.Activites.Leaves;
+import com.example.osamakhalid.schoolsystem.Adapters.LeavesAdapter;
+import com.example.osamakhalid.schoolsystem.BaseConnection.RetrofitInitialize;
+import com.example.osamakhalid.schoolsystem.ConnectionInterface.ClientAPIs;
+import com.example.osamakhalid.schoolsystem.Consts.Values;
+import com.example.osamakhalid.schoolsystem.GlobalCalls.CommonCalls;
+import com.example.osamakhalid.schoolsystem.Model.LeavesResponse;
+import com.example.osamakhalid.schoolsystem.Model.LeavesResponseList;
+import com.example.osamakhalid.schoolsystem.Model.LoginResponse;
 import com.example.osamakhalid.schoolsystem.R;
 
-/**
- * A simple {@link Fragment} subclass.
- * Activities that contain this fragment must implement the
- * {@link LeavesListFragment.OnFragmentInteractionListener} interface
- * to handle interaction events.
- * Use the {@link LeavesListFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
+import java.util.ArrayList;
+import java.util.List;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+import retrofit2.Retrofit;
+
+
 public class LeavesListFragment extends Fragment {
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
-
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
-
-    private OnFragmentInteractionListener mListener;
-
-    public LeavesListFragment() {
-        // Required empty public constructor
-    }
-
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment LeavesListFragment.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static LeavesListFragment newInstance(String param1, String param2) {
-        LeavesListFragment fragment = new LeavesListFragment();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
-
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
-    }
+    public RecyclerView recyclerView;
+    public RecyclerView.Adapter adapter;
+    public List<LeavesResponse> listItems;
+    private Retrofit retrofit;
+    private ClientAPIs clientAPIs;
+    LoginResponse userData;
+    ProgressDialog progressDialog;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_leaves_list2, container, false);
-    }
-
-    // TODO: Rename method, update argument and hook method into UI event
-    public void onButtonPressed(Uri uri) {
-        if (mListener != null) {
-            mListener.onFragmentInteraction(uri);
+        if (container != null) {
+            container.removeAllViews();
         }
+        View view = inflater.inflate(R.layout.fragment_leaves_list2, container, false);
+        listItems = new ArrayList<>();
+        progressDialog = CommonCalls.createDialouge(getActivity(), "", Values.DIALOGUE_MSG);
+        recyclerView = view.findViewById(R.id.leaves_recycler_view);
+        recyclerView.setHasFixedSize(true);
+        recyclerView.setLayoutManager(new LinearLayoutManager(getActivity()));
+        userData = CommonCalls.getUserData(getActivity());
+        String base = userData.getUsername() + ":" + userData.getPassword();
+        String authHeader = "Basic " + Base64.encodeToString(base.getBytes(), Base64.NO_WRAP);
+        getData(authHeader);
+        adapter = new LeavesAdapter(listItems, getActivity().getApplicationContext());
+        recyclerView.setAdapter(adapter);
+        return view;
     }
 
-    @Override
-    public void onAttach(Context context) {
-        super.onAttach(context);
-        if (context instanceof OnFragmentInteractionListener) {
-            mListener = (OnFragmentInteractionListener) context;
-        } else {
-            throw new RuntimeException(context.toString()
-                    + " must implement OnFragmentInteractionListener");
-        }
+    public void getData(String authHeader) {
+        retrofit = RetrofitInitialize.getApiClient();
+        clientAPIs = retrofit.create(ClientAPIs.class);
+        Call<LeavesResponseList> call = clientAPIs.getLeaves(userData.getUsername(), userData.getUsertype(), authHeader);
+        call.enqueue(new Callback<LeavesResponseList>() {
+            @Override
+            public void onResponse(Call<LeavesResponseList> call, Response<LeavesResponseList> response) {
+                if (response.isSuccessful()) {
+                    LeavesResponseList leavesList = response.body();
+                    if (leavesList != null && leavesList.getLeavesData() != null) {
+                        progressDialog.dismiss();
+                        listItems.addAll(leavesList.getLeavesData());
+                        adapter.notifyDataSetChanged();
+                    } else {
+                        progressDialog.dismiss();
+                        Toast.makeText(getActivity(), "Leaves not available yet.", Toast.LENGTH_SHORT).show();
+                    }
+                }
+            }
+
+            @Override
+            public void onFailure(Call<LeavesResponseList> call, Throwable t) {
+                progressDialog.dismiss();
+                Toast.makeText(getActivity(), "Sorry something went wrong.", Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 
-    @Override
-    public void onDetach() {
-        super.onDetach();
-        mListener = null;
-    }
 
-    /**
-     * This interface must be implemented by activities that contain this
-     * fragment to allow an interaction in this fragment to be communicated
-     * to the activity and potentially other fragments contained in that
-     * activity.
-     * <p>
-     * See the Android Training lesson <a href=
-     * "http://developer.android.com/training/basics/fragments/communicating.html"
-     * >Communicating with Other Fragments</a> for more information.
-     */
-    public interface OnFragmentInteractionListener {
-        // TODO: Update argument type and name
-        void onFragmentInteraction(Uri uri);
-    }
 }
