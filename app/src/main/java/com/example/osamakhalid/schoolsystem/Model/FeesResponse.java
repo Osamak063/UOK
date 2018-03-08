bb6343ddc4840c89a91a1349e3a6983a1ce2030c
Osamak063 <osamak063@gmail.com>
2018-03-08 19:54:49 +0500

    fees and invoice

--- app/src/main/java/com/example/osamakhalid/schoolsystem/Model/FeesResponse.java
+++ app/src/main/java/com/example/osamakhalid/schoolsystem/Model/FeesResponse.java
@@ -1,8 +1,88 @@
 package com.example.osamakhalid.schoolsystem.Model;
 
+import com.google.gson.annotations.Expose;
+import com.google.gson.annotations.SerializedName;
+
 /**
  * Created by Osama Khalid on 3/7/2018.
  */
 
 public class FeesResponse {
+    @SerializedName("fee_type")
+    @Expose
+    private String feeType;
+    @SerializedName("date")
+    @Expose
+    private String date;
+    @SerializedName("status")
+    @Expose
+    private String status;
+    @SerializedName("student")
+    @Expose
+    private String student;
+    @SerializedName("payment_type")
+    @Expose
+    private String paymentType;
+    @SerializedName("amount")
+    @Expose
+    private String amount;
+    @SerializedName("due_amount")
+    @Expose
+    private String dueAmount;
+
+    public String getFeeType() {
+        return feeType;
+    }
+
+    public void setFeeType(String feeType) {
+        this.feeType = feeType;
+    }
+
+    public String getDate() {
+        return date;
+    }
+
+    public void setDate(String date) {
+        this.date = date;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+    public String getStudent() {
+        return student;
+    }
+
+    public void setStudent(String student) {
+        this.student = student;
+    }
+
+    public String getPaymentType() {
+        return paymentType;
+    }
+
+    public void setPaymentType(String paymentType) {
+        this.paymentType = paymentType;
+    }
+
+    public String getAmount() {
+        return amount;
+    }
+
+    public void setAmount(String amount) {
+        this.amount = amount;
+    }
+
+    public String getDueAmount() {
+        return dueAmount;
+    }
+
+    public void setDueAmount(String dueAmount) {
+        this.dueAmount = dueAmount;
+    }
 }
