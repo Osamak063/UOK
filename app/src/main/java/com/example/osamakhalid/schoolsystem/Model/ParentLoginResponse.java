a757f91fb571dbf70d398dfafaecc8d75ebbd006
Osamak063 <osamak063@gmail.com>
2018-03-19 00:59:23 +0500

    parent login

--- app/src/main/java/com/example/osamakhalid/schoolsystem/Model/ParentLoginResponse.java
+++ app/src/main/java/com/example/osamakhalid/schoolsystem/Model/ParentLoginResponse.java
@@ -1,8 +1,242 @@
 package com.example.osamakhalid.schoolsystem.Model;
 
+import com.google.gson.annotations.Expose;
+import com.google.gson.annotations.SerializedName;
+
 /**
  * Created by Osama Khalid on 3/18/2018.
  */
 
 public class ParentLoginResponse {
+    @SerializedName("parentID")
+    @Expose
+    private String parentID;
+    @SerializedName("name")
+    @Expose
+    private String name;
+    @SerializedName("father_name")
+    @Expose
+    private String fatherName;
+    @SerializedName("mother_name")
+    @Expose
+    private String motherName;
+    @SerializedName("father_profession")
+    @Expose
+    private String fatherProfession;
+    @SerializedName("mother_profession")
+    @Expose
+    private String motherProfession;
+    @SerializedName("email")
+    @Expose
+    private String email;
+    @SerializedName("phone")
+    @Expose
+    private String phone;
+    @SerializedName("address")
+    @Expose
+    private String address;
+    @SerializedName("photo")
+    @Expose
+    private String photo;
+    @SerializedName("username")
+    @Expose
+    private String username;
+    @SerializedName("password")
+    @Expose
+    private String password;
+    @SerializedName("usertype")
+    @Expose
+    private String usertype;
+    @SerializedName("create_date")
+    @Expose
+    private String createDate;
+    @SerializedName("modify_date")
+    @Expose
+    private String modifyDate;
+    @SerializedName("create_userID")
+    @Expose
+    private String createUserID;
+    @SerializedName("create_username")
+    @Expose
+    private String createUsername;
+    @SerializedName("create_usertype")
+    @Expose
+    private String createUsertype;
+    @SerializedName("parentactive")
+    @Expose
+    private String parentactive;
+    @SerializedName("status")
+    @Expose
+    private Integer status;
+    @SerializedName("image_url")
+    @Expose
+    private String imageUrl;
+
+    public String getParentID() {
+        return parentID;
+    }
+
+    public void setParentID(String parentID) {
+        this.parentID = parentID;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getFatherName() {
+        return fatherName;
+    }
+
+    public void setFatherName(String fatherName) {
+        this.fatherName = fatherName;
+    }
+
+    public String getMotherName() {
+        return motherName;
+    }
+
+    public void setMotherName(String motherName) {
+        this.motherName = motherName;
+    }
+
+    public String getFatherProfession() {
+        return fatherProfession;
+    }
+
+    public void setFatherProfession(String fatherProfession) {
+        this.fatherProfession = fatherProfession;
+    }
+
+    public String getMotherProfession() {
+        return motherProfession;
+    }
+
+    public void setMotherProfession(String motherProfession) {
+        this.motherProfession = motherProfession;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getPhoto() {
+        return photo;
+    }
+
+    public void setPhoto(String photo) {
+        this.photo = photo;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getUsertype() {
+        return usertype;
+    }
+
+    public void setUsertype(String usertype) {
+        this.usertype = usertype;
+    }
+
+    public String getCreateDate() {
+        return createDate;
+    }
+
+    public void setCreateDate(String createDate) {
+        this.createDate = createDate;
+    }
+
+    public String getModifyDate() {
+        return modifyDate;
+    }
+
+    public void setModifyDate(String modifyDate) {
+        this.modifyDate = modifyDate;
+    }
+
+    public String getCreateUserID() {
+        return createUserID;
+    }
+
+    public void setCreateUserID(String createUserID) {
+        this.createUserID = createUserID;
+    }
+
+    public String getCreateUsername() {
+        return createUsername;
+    }
+
+    public void setCreateUsername(String createUsername) {
+        this.createUsername = createUsername;
+    }
+
+    public String getCreateUsertype() {
+        return createUsertype;
+    }
+
+    public void setCreateUsertype(String createUsertype) {
+        this.createUsertype = createUsertype;
+    }
+
+    public String getParentactive() {
+        return parentactive;
+    }
+
+    public void setParentactive(String parentactive) {
+        this.parentactive = parentactive;
+    }
+
+    public Integer getStatus() {
+        return status;
+    }
+
+    public void setStatus(Integer status) {
+        this.status = status;
+    }
+
+    public String getImageUrl() {
+        return imageUrl;
+    }
+
+    public void setImageUrl(String imageUrl) {
+        this.imageUrl = imageUrl;
+    }
 }
