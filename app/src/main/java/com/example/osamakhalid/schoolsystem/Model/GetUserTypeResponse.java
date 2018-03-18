dc2dd0abda21da30c666e268ba1766ef4ecbec62
Osamak063 <osamak063@gmail.com>
2018-03-19 01:00:04 +0500

    parent login

---
+++ app/src/main/java/com/example/osamakhalid/schoolsystem/Model/GetUserTypeResponse.java
@@ -0,0 +1,22 @@
+package com.example.osamakhalid.schoolsystem.Model;
+
+import com.google.gson.annotations.Expose;
+import com.google.gson.annotations.SerializedName;
+
+/**
+ * Created by Osama Khalid on 3/13/2018.
+ */
+
+public class GetUserTypeResponse {
+    @SerializedName("usertype")
+    @Expose
+    private String usertype;
+
+    public String getUsertype() {
+        return usertype;
+    }
+
+    public void setUsertype(String usertype) {
+        this.usertype = usertype;
+    }
+}
